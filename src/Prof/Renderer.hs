3f6043b810da7ef1cf76f9c1c6ca95e975d99bec
Rob Rix <rob.rix@me.com>
2017-06-19 18:07:18 -0400

    Size everything.

--- src/Prof/Renderer.hs
+++ src/Prof/Renderer.hs
@@ -5,8 +5,11 @@ import qualified Data.ByteString.Lazy as B hiding (unpack)
 import qualified Data.ByteString.Char8 as B (unpack)
 import Data.Char
 import Data.Foldable (foldl', for_)
+import Data.Function
 import qualified Data.IntMap as Map
 import Data.List as List
+import Data.List.NonEmpty (nonEmpty)
+import Data.Ord
 import Data.Semigroup
 import Profiling.Heap.Read (readProfile)
 import Profiling.Heap.Types
@@ -30,10 +33,14 @@ renderProfile Profile{..} = H.docTypeHtml $ do
       for_ (Map.toList prNames) $ \ (costCentreId, name) ->
         H.li ! A.id_ (toValue costCentreId) $ string (B.unpack name)
     H.div ! AH.class_ (toValue "graph") $
-      S.svg ! S.customAttribute (S.stringTag "xmlns") (S.toValue "http://www.w3.org/2000/svg") ! S.customAttribute (S.stringTag "xmlns:xlink") (S.toValue "http://www.w3.org/1999/xlink") ! A.width (toValue (maybe (0 :: Int) ((* 60) . ceiling . fst . fst) (uncons prSamples))) $ do
+      S.svg
+      ! S.customAttribute (S.stringTag "xmlns") (S.toValue "http://www.w3.org/2000/svg")
+      ! S.customAttribute (S.stringTag "xmlns:xlink") (S.toValue "http://www.w3.org/1999/xlink")
+      ! A.width (toValue (maybe (60 :: Int) ((* 60) . ceiling . fst . fst) (uncons prSamples)))
+      ! A.height (toValue (maybe (60 :: Int) ((* 60) . ceiling . (* (1/1024/1024)) . fromIntegral . maximum . fmap (snd . maximumBy (compare `on` snd) . snd)) (nonEmpty prSamples))) $ do
         S.title $ S.string (prJob <> " " <> prDate)
 
-        S.g ! A.transform (S.scale 60 (10/1024/1024)) $
+        S.g ! A.transform (S.scale 60 (60/1024/1024)) $
           foldr (>>) (pure ()) $ Map.mapWithKey toPath . Map.unionsWith (<>) . fmap (fmap (:[])) $ zipWith toMap [0..] (reverse prSamples)
   where toPath :: CostCentreId -> [(Int, Time, Double)] -> S.Svg
         toPath costCentreId points = S.path ! A.d (S.mkPath (snd (foldl' step (pred 0, S.m 0 0) points))) ! A.id_ (S.toValue costCentreId)
