9354b1a3e2a97541d734905add118074f8cbeb1d
Rob Rix <rob.rix@me.com>
2017-06-20 11:37:25 -0400

    Toggle paths.

--- legend.js
+++ legend.js
@@ -18,6 +18,17 @@ var Hover = function (element, target) {
   element.addEventListener('click', this);
 };
 
+var Toggle = function(element, target) {
+  this.handleEvent = event => {
+    if (target.classList.contains("hidden")) {
+      target.classList.remove("hidden");
+    } else {
+      target.classList.add("hidden");
+    }
+  };
+  element.addEventListener('change', this);
+};
+
 function run() {
   for (let li of document.querySelectorAll("#legend li")) {
     let costCentreId = li.getAttribute("data-id");
@@ -28,5 +39,11 @@ function run() {
     let costCentreId = path.getAttribute("data-id");
     let li = document.querySelector("#legend-" + costCentreId);
     path.hover = new Hover(path, li);
+  }
+
+  for (let input of document.querySelectorAll("#legend li input")) {
+    let costCentreId = input.getAttribute("data-id");
+    let path = document.querySelector("#path-" + costCentreId);
+    input.toggle = new Toggle(input, path);
   }
 }
